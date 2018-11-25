45301f63dce75c0ee0bdbdceecd4ed453d0135ce
Florian Bauer <florian@moonshiner.at>
2018-11-25 20:00:29 +0100

    Add Presentation Possibility

--- src/pages/Presentation/Presentation.js
+++ src/pages/Presentation/Presentation.js
@@ -2,6 +2,20 @@ import React, { useState, useEffect } from "react";
 import styles from "./Presentation.module.scss";
 import { Link } from "react-router-dom";
 
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text
+} from "spectacle";
+import createTheme from "spectacle/lib/themes/default";
+
 export default function() {
   const [message, setMessage] = useState("waiting");
   useEffect(() => {
@@ -16,11 +30,30 @@ export default function() {
       setMessage(evt.data);
     };
   }, []);
+
+  const theme = createTheme(
+    {
+      primary: "white",
+      secondary: "#1F2022",
+      tertiary: "#03A9FC",
+      quaternary: "#CECECE"
+    },
+    {
+      primary: "Montserrat",
+      secondary: "Helvetica"
+    }
+  );
+
   return (
-    <div className={styles.container}>
-      <Link to="/">zur Kamera</Link>
-      <br />
-      {message}
-    </div>
+    <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+      <Slide transition={["zoom"]} bgColor="primary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          AI on Automation Infrastructure
+        </Heading>
+      </Slide>
+      <Slide transition={["zoom"]} bgColor="primary">
+        <Link to="/">zur Kamera {message}</Link>
+      </Slide>
+    </Deck>
   );
 }
